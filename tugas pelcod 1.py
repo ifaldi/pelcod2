e38e2ce29635a523ddb8434323199aa408c60c25
ifaldi <143860016+ifaldi@users.noreply.github.com>
2023-09-08 20:10:09 +0700

    Add files via upload
    Ifaldi hernawan

---
+++ tugas pelcod 1.py
@@ -0,0 +1,24 @@
+#rumus_menghitung_volume_tabung
+
+r =int(input("masukkan jari-jari tabung = "))
+t =int(input("masukkan tinggi tabung = "))
+phi = 22/7
+
+volume = phi * r * r * t
+print("volume = ", volume )
+
+#rumus_menghitung_volume_bola
+
+r =int(input("masukkan jari-jari bola = "))
+phi = 22/7
+volume = 4 * phi * r * r
+print("volume = ", volume )
+
+#rumus_menghitung_volume_balok
+
+panjang = int(input("masukkan panjang balok = "))
+lebar = int(input("masukkan lebar balok = "))
+tinggi = int(input("masukkan tinggi balok = "))
+
+volume = panjang * lebar * tinggi
+print("volume = ", volume )
